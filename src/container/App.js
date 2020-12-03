2f832a6d6a476d82e35af062d3520ed6a581ff2b
SobhanBera <sobhanbera258@gmail.coms>
2020-12-03 12:09:21 +0530

    added app and mainApp

--- src/container/App.js
+++ src/container/App.js
@@ -3,7 +3,6 @@ import MainApp from "./MainApp";
 import { BrowserRouter, Switch } from "react-router-dom";
 
 import ErrorBoundary from "../components/ErrorBoundary/ErrorBoundary";
-import ChildErrorBoundary from "../components/ErrorBoundary/ChildErrorBoundary";
 
 function App() {
 	return (
--- src/container/MainApp.js
+++ src/container/MainApp.js
@@ -1,27 +1,55 @@
 import React, { Component } from "react";
-import styles, { extraStyles } from "../hoc/Css";
 
 import { Route, Switch } from "react-router-dom";
 
 import Auxiliary from "../hoc/Auxiliary";
 import ChildErrorBoundary from "../components/ErrorBoundary/ChildErrorBoundary";
 import Header from "../components/Header/Header";
+import Loader from "../components/loader/Loader";
+import Auth from "../components/auth/Authentication";
+import Footer from "../components/footer/Footer";
+import firebase from "../firebase/Firebase";
 
 class MainApp extends Component {
 	constructor() {
 		super();
 		this.state = {
 			lightTheme: true,
+			showLoader: false,
+			isLoggedIn: false,
+			user: "",
 		};
+		// no need of this below line...
+		this.toggleTheme = this.toggleTheme.bind(this);
+		this.disableLoader = this.disableLoader.bind(this);
 	}
 
-	toggleTheme() {
+	toggleTheme = () => {
 		this.setState((prev) => {
 			return {
 				lightTheme: !prev.lightTheme,
 			};
 		});
-	}
+	};
+
+	disableLoader = () => {
+		this.setState({
+			showLoader: false,
+		});
+	};
+
+	reloadFuntion = () => {
+		console.log(this.state.user);
+		firebase.auth().onAuthStateChanged((userO) => {
+			if (userO) {
+				this.setState({
+					user: userO,
+				});
+				console.log(this.state.user);
+			} else {
+			}
+		});
+	};
 
 	render() {
 		const currTheme = this.state.lightTheme ? "light" : "dark";
@@ -29,9 +57,26 @@ class MainApp extends Component {
 		return (
 			<ChildErrorBoundary>
 				<Auxiliary>
-					<div className={`${currTheme} main-app-start_sbHcTtWwfDOt`}>
-						<Header theme={currTheme} />
-						<Switch></Switch>
+					<div
+						className={`${currTheme} main-app-start_sbHcTtWwfDOt`}
+						onLoad={this.disableLoader}
+					>
+						<Header theme={currTheme} profileImg={""} />
+
+						{this.state.isLoggedIn ? (
+							<Switch>
+								<Route path="*" component={""} />
+								<Route exact path="/" component={""} />
+								<Route exact path="/dashboard" component={""} />
+							</Switch>
+						) : (
+							<Auth reloadFuntion={this.reloadFuntion} />
+						)}
+
+						<Footer />
+
+						{/* EXTRAS */}
+						{this.state.showLoader ? <Loader /> : null}
 					</div>
 				</Auxiliary>
 			</ChildErrorBoundary>
