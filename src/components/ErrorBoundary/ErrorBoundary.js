4c3aa020eb919c90650f9403ee8dc13082ecea3a
SobhanBera <sobhanbera258@gmail.coms>
2020-12-10 15:49:28 +0530

    added other components

--- src/components/ErrorBoundary/ErrorBoundary.js
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -31,4 +31,34 @@ class ErrorBoundary extends Component {
 	}
 }
 
+class ChildErrorBoundary extends Component {
+	constructor() {
+		super();
+		this.state = {
+			errorFound: false,
+			error: "",
+		};
+	}
+
+	static getDerivedStateFromError(error) {
+		// Update state so the next render will show the fallback UI.
+		return { errorFound: true };
+	}
+
+	componentDidCatch(err, errlog) {
+		this.setState({
+			errorFound: true,
+			error: err,
+		});
+	}
+
+	render() {
+		if (this.state.errorFound) {
+			return <div style={{ color: "#ef4040" }}>Some Error Occurred.</div>;
+		}
+		return this.props.children;
+	}
+}
+
 export default ErrorBoundary;
+export { ChildErrorBoundary };
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,66 +1,66 @@
-import styles from "../../hoc/Css";
+import React from "react";
+import styles from "../elements/styles/style";
 
-const Footer = (props) => {
+function Footer(props) {
 	return (
-		<div className={styles.mainFooterEntry}>
-			<div className={styles.footer}>
-				<div className={styles.footerHeader}>
-					<h3>
-						<a href="https://telebyteweb.herokuapp.com">TeleByte</a>
-					</h3>
-					<span>
+		<div className={styles.FooterStart}>
+			<div className={styles.MainFooter}>
+				<div className={styles.FooterHeading}>
+					<h2>TeleByte</h2>
+					<p>
 						TeleByte provides a developer account to you. Talk with
 						other developer out there, invite others. Collaborate
 						together, contribute together.
-					</span>
+					</p>
 				</div>
-
-				<div className={styles.footerSections}>
-					<div className={styles.footerSection}>
-						<h4>Developer's Social Media</h4>
-						<ul>
-							<li>
-								<a href="https://www.linkedin.com/in/sobhanbera">
-									{" "}
-									Linkedin
-								</a>
-							</li>
-							<li>
-								<a href="https://www.facebook.com/sobhan.b.90/">
-									Facebook
-								</a>
-							</li>
-							<li>
-								<a href="https://www.instagram.com/sobhanbera_/">
-									Instagram
-								</a>
-							</li>
-						</ul>
-					</div>
-					<div className={styles.footerSection}>
-						<h4>More</h4>
-						<ul>
-							<li>
-								<a href="https://github.com/SobhanBera">
-									GitHub
-								</a>
-							</li>
-							<li>
-								<a href="https://dribbble.com/sobhanbera">
-									Dribble
-								</a>
-							</li>
-							<li>
-								<a href="https://sobhanbera.github.io/portfolio">
-									Portfolio
-								</a>
-							</li>
-						</ul>
-					</div>
+				<div className={styles.FooterSection}>
+					<h4>Developer's Social Media</h4>
+					<ul>
+						<li>
+							<a href="https://www.linkedin.com/in/sobhanbera">
+								Linkedin
+							</a>
+						</li>
+						<li>
+							<a href="https://www.facebook.com/sobhan.b.90/">
+								Facebook
+							</a>
+						</li>
+						<li>
+							<a href="https://www.instagram.com/sobhanbera_/">
+								Instagram
+							</a>
+						</li>
+					</ul>
 				</div>
+				<div className={styles.FooterSection}>
+					<h4>More</h4>
+					<ul>
+						<li>
+							<a href="https://github.com/SobhanBera">GitHub</a>
+						</li>
+						<li>
+							<a href="https://dribbble.com/sobhanbera">
+								Dribble
+							</a>
+						</li>
+						<li>
+							<a href="https://sobhanbera.github.io/portfolio">
+								Portfolio
+							</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<div className={styles.FooterContribute}>
+				<p>
+					<a href="https://github.com/SobhanBera/telebyteweb">
+						GitHub Repository
+					</a>
+				</p>
 			</div>
 		</div>
 	);
-};
+}
 
 export default Footer;
