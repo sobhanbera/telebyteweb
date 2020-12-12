15f5db188c2ab5c6e343883a55a30be79a89d653
SobhanBera <sobhanbera258@gmail.coms>
2020-12-12 12:47:50 +0530

    added just features

--- src/components/MainSection/dashboardSection/Dashboard.js
+++ src/components/MainSection/dashboardSection/Dashboard.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class Dashboard extends Component {
+	componentDidMount() {
+		document.title = "Telebyte | Dashboard";
+	}
+
 	render() {
 		return <div>Dashboard</div>;
 	}
